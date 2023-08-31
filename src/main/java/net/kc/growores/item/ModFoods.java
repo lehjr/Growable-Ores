f51f9ec61ef4f4c3833732eed12151a318b79ec0
XsquadKc <44047752+XsquadKc@users.noreply.github.com>
2023-08-31 19:49:41 +1000

    1.20.1-1.0.5

--- src/main/java/net/kc/growores/item/ModFoods.java
+++
@@ -1,9 +0,0 @@
-package net.kc.growores.item;
-
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.food.FoodProperties;
-
-public class ModFoods {
-
-}
